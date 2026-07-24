c028d1b5a454b7717a05ba78e9af425055a5eaeb
wikai <swikirai@gmail.com>
2026-07-24 12:25:43 +0300

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,18 @@
+from random import randint
+
+number = randint(1, 100)
+print('Угадайте число от 1 до 100')
+
+while True:
+
+    guess = int(input('Введите число:'))
+    
+    if guess < number:
+        print('Ваше число меньше загаданного')
+    
+    elif guess > number:
+        print('Ваше число больше загаданного')
+    
+    else:
+        break
+print('Вы угадали!')
